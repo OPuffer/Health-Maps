6a8d4c32df22a360937d41c0af9ba39871fcfa71
OPuffer <pufferoliver@gmail.com>
2022-11-05 16:34:03 -0700

    some minor adjustments

--- index.js
+++ index.js
@@ -2,15 +2,15 @@
 function initMap() {
     console.log("hi");
     // The location of Uluru
-    const SF = { lat: 37.7749, lng: -122.4194};
+    const SF = { lat: 37.77, lng: -122.419};
     // The map, centered at Uluru
     const map = new google.maps.Map(document.getElementById("map"), {
-      zoom: 4,
+      zoom: 12,
       center: SF,
     });
     // The marker, positioned at Uluru
     // const marker = new google.maps.Marker({
-    //   position: uluru,
+    //   position: SF,
     //   map: map,
     // });
   }
